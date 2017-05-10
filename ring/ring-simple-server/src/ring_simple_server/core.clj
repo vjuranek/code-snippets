5e9d6f6cc772a039f995352346abe0e9a78c7fc6
Vojtech Juranek <vojtech.juranek@gmail.com>
2017-05-10 15:22:29 +0200

    Create simple server using ring and hiccup

---
+++ ring/ring-simple-server/src/ring_simple_server/core.clj
@@ -0,0 +1,28 @@
+(ns ring-simple-server.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as res]
+            [hiccup.page :as p]))
+
+(defn render-page [request]
+  (p/html5
+   [:head
+    [:title "Ring and hiccup test"]]
+   [:body
+    [:h1 "Hello world!"]]
+  )
+)
+
+(defn gen-res
+  [page-content]
+  (-> (res/response page-content)
+      (res/content-type "text/html")))
+
+(defn handler [request]
+  (gen-res(render-page request)))
+
+(defn start-server [port]
+  (println "Starting Jetty on port " port)
+  (jetty/run-jetty handler {:port port}))
+
+(defn -main []
+  (start-server 3000))
