c44590be8090daf9fe03ebb493ba1d4bd81a8948
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-01-17 20:37:43 +0100

    Add CRUD operations

--- db/sqlite/python/basic.py
+++ db/sqlite/python/basic.py
@@ -8,14 +8,25 @@ TABLE_NAME="my_table"
 conn = sqlite3.connect(DB_FILE)
 c = conn.cursor()
 
-c.execute("CREATE TABLE IF NOT EXISTS {tn} (id INTEGER PRIMARY KEY, msg TEXT)".format(tn=TABLE_NAME))
+c.execute("CREATE TABLE IF NOT EXISTS {tn} (id INTEGER PRIMARY KEY, msg TEXT, msg2 TEXT)".format(tn=TABLE_NAME))
 conn.commit()
 
-c.execute("INSERT INTO {tn}(id, msg) VALUES (?, ?)".format(tn=TABLE_NAME), (1, "hello world1"))
-conn.commit()
+# c.execute("INSERT INTO {tn}(id, msg) VALUES (?, ?)".format(tn=TABLE_NAME), (1, "hello1"))
+# c.execute("INSERT INTO {tn}(id, msg) VALUES (?, ?)".format(tn=TABLE_NAME), (2, "hello2"))
+# c.execute("INSERT INTO {tn}(id, msg, msg2) VALUES (?, ?, ?)".format(tn=TABLE_NAME), (3, "hello3", "world"))
+# c.execute("INSERT INTO {tn}(id, msg, msg2) VALUES (?, ?, ?)".format(tn=TABLE_NAME), (4, "hello4", "world"))
+# conn.commit()
 
-res = c.execute("SELECT * FROM {tn}".format(tn=TABLE_NAME))
+
+# c.execute("DELETE FROM {tn} WHERE id = ?".format(tn=TABLE_NAME), (2,))
+
+# c.execute("UPDATE {tn} SET msg = ? WHERE id = ?".format(tn=TABLE_NAME), ("hello1 update", 1)) 
+
+res_cur = c.execute("SELECT id, msg FROM {tn} WHERE id = ?".format(tn=TABLE_NAME), (1,))
+res = res_cur.fetchall()
+print(len(res))
 for r in res:
     print(r)
+    print(r[1])
 
 conn.close()
