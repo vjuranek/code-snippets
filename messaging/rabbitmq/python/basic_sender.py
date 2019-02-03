4f14db55b026573bb4461fa8177838de21849aaa
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-02-03 22:14:20 +0100

    Use queue name constant

--- messaging/rabbitmq/python/basic_sender.py
+++ messaging/rabbitmq/python/basic_sender.py
@@ -6,7 +6,7 @@ QUEUE_NAME = "test"
 
 with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as conn:
     ch = conn.channel()
-    ch.queue_declare(queue='test')
+    ch.queue_declare(queue=QUEUE_NAME)  # ensure queue exists
 
-    ch.basic_publish(exchange='', routing_key='test', body='Hello World!')
+    ch.basic_publish(exchange='', routing_key=QUEUE_NAME, body='Hello World!')
     print("Sent 'Hello World!'")
