1f13ee8ccd76a2d1a88b80dee800aa55be877ecf
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-02-07 21:24:25 +0100

    RabbitMQ receiver QoS

---
+++ messaging/rabbitmq/python/receiver_qos.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import pika
+
+from durable_queue import QUEUE_NAME
+
+
+def rec_callback(channel, method, properties, body):
+    print("Received {}".format(body))
+    print("Sending message ACK")
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as conn:
+    ch = conn.channel()
+    ch.queue_declare(queue=QUEUE_NAME, durable=True)  # ensure queue exists
+
+    # setup receiver to get new msq after is done (sent ack) for previous
+    ch.basic_qos(prefetch_count=1)
+    ch.basic_consume(rec_callback, queue=QUEUE_NAME)
+
+    print("Waiting for messages. To exit press CTRL+C")
+    ch.start_consuming()
