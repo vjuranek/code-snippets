d7ce2aa1d4e6ebaa15ec769d0b0bfd61fb30e469
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-03-17 00:15:33 +0100

    File read

--- linux-sys/io/file.c
+++ linux-sys/io/file.c
@@ -1,16 +1,18 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void write_file() {
-  int fd;
 
-  fd = open("/tmp/test", O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH);
+extern int errno;
+
+int write_file() {
+  int fd = open("/tmp/test", O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH);
   if (fd == -1) {
     perror("cannot open file");
   }
@@ -18,8 +20,7 @@ void write_file() {
   const char *buf = "hello world!";
 
   int len = strlen(buf);
-  ssize_t nw;
-  extern int errno;
+  ssize_t nw, written = 0;
   
   while (len != 0 && (nw = write(fd, buf, len)) != 0) {
     if (nw == -1) {
@@ -31,15 +32,49 @@ void write_file() {
 
     len -= nw;
     buf += nw;
+    written += nw;
   }
   
   if (close(fd) == -1) {
     perror("cannot close file");
   }
+  
+  return written;
+}
+
+void read_file(int len) {
+  int fd = open("/tmp/test", O_RDONLY);
+  if (fd == -1) {
+    perror("cannot open file");
+  }
+
+  char *buf = (char *) malloc(256 * sizeof(char));
+  ssize_t nr, nread = 0;
+  
+  while (len != 0 && (nr = read(fd, buf, len)) != 0) {
+    if (nr == -1) {
+      if (errno == EINTR) { // signal received before any bytes were read and call can be reissued
+	continue;
+      }
+      perror("cannot read from file");
+    }
+
+    len -= nr;
+    buf += nr;
+    nread += nr;
+  }
+
+  if (close(fd) == -1) {
+    perror("cannot close file");
+  }
+
+  buf -= nread; 
+  printf("read:\n%s", buf);
 }
 
 int main() {
-  write_file();
+  int written = write_file();
+  printf("written %d bytes\n", written);
+  read_file(written);
 }
-
-  
+ 
