78332bf94c5fdd096c3c3ba0d821cdb88aff5e6c
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-04-01 14:56:08 +0200

    WIP: direct io write

---
+++ linux-sys/io/file_direct.c
@@ -0,0 +1,49 @@
+// man open says:
+// "The O_DIRECT, O_NOATIME, O_PATH, and O_TMPFILE flags are Linux-specific.  One must define _GNU_SOURCE to obtain their definitions."
+// Or compile with gcc -D_GNU_SOURCE
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+extern int errno;
+
+int write_file_direct() {
+  //ssize_t pagesize = getpagesize();
+  //printf("pagesize: %d\n", pagesize);
+  
+  int fd = open("/tmp/test", O_WRONLY | O_CREAT | O_TRUNC | O_DIRECT);
+  if (fd == -1) {
+    perror("cannot open file");
+  }
+
+  const char buf[4096] = "hello world!";
+
+  int len = strlen(buf);
+  ssize_t nw = 0;
+
+  nw = write(fd, buf, len);
+  if (nw == -1) {
+    perror("cannot write to file");
+  }
+  
+  if (close(fd) == -1) {
+    perror("cannot close file");
+  }
+  
+  return nw;
+}
+
+int main() {
+  int written = write_file_direct();
+  printf("written %d bytes\n", written);
+}
+ 
