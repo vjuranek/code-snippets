92aae7bae489411b8431450746800e22f061a646
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-02-05 21:57:11 +0100

    Update ring and clojure dependencies

--- ring/ring-simple-server/project.clj
+++ ring/ring-simple-server/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.0"]
                  [hiccup "1.0.5"]]
   :main ring-simple-server.core)
