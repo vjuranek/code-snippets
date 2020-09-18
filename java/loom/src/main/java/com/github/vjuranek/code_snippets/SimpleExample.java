f830ad03010f5f8164190edbfb458b28eb644f61
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-09-18 14:40:18 +0200

    loom: simple thread example

--- java/loom/src/main/java/com/github/vjuranek/code_snippets/SimpleExample.java
+++ java/loom/src/main/java/com/github/vjuranek/code_snippets/SimpleExample.java
@@ -6,6 +6,15 @@ import java.util.concurrent.Executors;
 
 public class SimpleExample {
     public static void main(String[] args) throws Exception {
+	virtThread();
+    }
+
+    private static void virtThread() throws Exception {
+	var thread = Thread.startVirtualThread(() -> System.out.println("Virtual thread!"));
+	thread.join();
+    }
+    
+    private static void virtExecutorService() throws Exception {
 	try (var e  = Executors.newVirtualThreadExecutor()) {
 	    List<Callable<String>> tasks = List.of(
 	        () -> "a",
