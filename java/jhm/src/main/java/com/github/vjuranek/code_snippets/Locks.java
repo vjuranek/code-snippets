fcf63f1dbd1e7b88f130abd669a5c048c974f9ff
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-09-26 22:06:41 +0200

    jhm: locks

---
+++ java/jhm/src/main/java/com/github/vjuranek/code_snippets/Locks.java
@@ -0,0 +1,58 @@
+package com.github.vjuranek.code_snippets;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Based on https://shipilev.net/jvm/anatomy-quarks/1-lock-coarsening-for-loops/
+ */
+
+@Warmup(iterations = 10, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(value = 1, jvmArgsPrepend = {"-XX:-UseBiasedLocking"})
+//@Fork(value = 1, jvmArgsPrepend = {"-XX:-UseBiasedLocking", "-XX:LoopUnrollLimit=1"})
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class Locks {
+
+    int x = 0;
+    int inc = 1;
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void forLock() {
+        for (int c = 0; c < 1000; c++) {
+            synchronized (this) {
+                x += inc;
+            }
+        }
+    }
+
+    @Benchmark
+    public void forLockInlined() {
+        for (int c = 0; c < 1000; c++) {
+            synchronized (this) {
+                x += inc;
+            }
+        }
+    }
+
+    @Benchmark
+    public void forLockGlob() {
+        synchronized (this) {
+            for (int c = 0; c < 1000; c++) {
+                x += inc;
+            }
+        }
+    }
+
+    @Benchmark
+    public void forNoLocks() {
+        for (int c = 0; c < 1000; c++) {
+            x += inc;
+        }
+    }
+
+}
