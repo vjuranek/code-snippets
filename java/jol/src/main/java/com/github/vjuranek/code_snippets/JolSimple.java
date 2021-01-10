93d616fab47693acb4ab2f6dca8213a5da600d7f
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-01-10 23:13:42 +0100

    jol: add layout of class with long and boolean

--- java/jol/src/main/java/com/github/vjuranek/code_snippets/JolSimple.java
+++ java/jol/src/main/java/com/github/vjuranek/code_snippets/JolSimple.java
@@ -7,12 +7,23 @@ public class JolSimple {
     public static void main(String[] args) {
         System.out.println(VM.current().details());
         System.out.println(ClassLayout.parseClass(ClassWithBoolean.class).toPrintable());
+        System.out.println(ClassLayout.parseClass(ClassWithLong.class).toPrintable());
+        System.out.println(ClassLayout.parseClass(ClassWithBooleanAndLong.class).toPrintable());
         System.out.println(ClassLayout.parseClass(ClassWithString.class).toPrintable());
         System.out.println(ClassLayout.parseClass(String.class).toPrintable());
     }
     
     public static class ClassWithBoolean {
         boolean f;
+    }
+
+    public static class ClassWithLong {
+        Long f;
+    }
+
+    public static class ClassWithBooleanAndLong {
+        Boolean b;
+        Long f;
     }
     
     public static class ClassWithString {
