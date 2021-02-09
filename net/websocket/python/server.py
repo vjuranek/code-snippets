771fa41c4ab4bbf22b293c3d6a348c8fbe9179e9
Vojtech Juranek <vojtech.juranek@gmail.com>
2021-02-09 21:51:54 +0100

    websockets: stop server on keyboard interrupt

--- net/websocket/python/server.py
+++ net/websocket/python/server.py
@@ -7,7 +7,20 @@ async def hello(conn, path):
     num = await conn.recv()
     await conn.send(f"Hello client #{num}!")
 
-start_server = websockets.serve(hello, "localhost", 8000)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+def start_server():
+    start_server = websockets.serve(hello, "localhost", 8000)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+def stop_server():
+    asyncio.get_event_loop().stop()
+    
+    
+if __name__ == '__main__':
+    try:
+        start_server()
+    except KeyboardInterrupt:
+        stop_server()
+
