29db2f67e76b3fa778219fa09640e9293816f032
Vojtech Juranek <vojtech.juranek@gmail.com>
2023-05-09 22:06:20 +0200

    tracing: add context propagation

--- tracing/opentelemetry/java/counter-proxy/src/main/java/CounterProxy.java
+++ tracing/opentelemetry/java/counter-proxy/src/main/java/CounterProxy.java
@@ -1,4 +1,11 @@
-import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapSetter;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -16,37 +23,60 @@ import java.net.http.HttpResponse;
 public class CounterProxy {
     private static final Logger LOG = Logger.getLogger(CounterProxy.class);
 
+    @Inject
+    private OpenTelemetry openTelemetry;
+
+    @Inject
+    private Tracer tracer;
+
     private HttpClient client = HttpClient.newBuilder().build();
+    private TextMapSetter<HttpRequest.Builder> setter = new TextMapSetter<>() {
+        @Override
+        public void set(HttpRequest.Builder carrier, String key, String value) {
+            carrier.header(key, value);
+        }
+    };
 
     @GET
-    @WithSpan
     @Produces(MediaType.TEXT_PLAIN)
     public Response count() throws Exception {
         LOG.info("hello called");
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("http://counter:8080/counter"))
-                .GET()
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return Response
-                .status(response.statusCode())
-                .entity(String.format("Counter state: %s", response.body()))
-                .build();
+
+        HttpRequest.Builder reqBuilder = HttpRequest.newBuilder().uri(new URI("http://counter:8080/counter")).GET();
+        Span clientSpan = tracer.spanBuilder("GET proxy/counter").setSpanKind(SpanKind.CLIENT).startSpan();
+
+        try (Scope scope = clientSpan.makeCurrent()) {
+            openTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), reqBuilder, setter);
+            HttpResponse<String> response = client.send(reqBuilder.build(), HttpResponse.BodyHandlers.ofString());
+            return Response
+                    .status(response.statusCode())
+                    .entity(String.format("Counter state: %s", response.body()))
+                    .build();
+        } finally {
+            clientSpan.end();
+        }
     }
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     public Response increment() throws Exception {
         LOG.info(String.format("increment called"));
-        HttpRequest request = HttpRequest.newBuilder()
+
+        HttpRequest.Builder reqBuilder = HttpRequest.newBuilder()
                 .uri(new URI("http://counter:8080/counter"))
-                .POST(HttpRequest.BodyPublishers.ofString(""))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return Response
-                .status(response.statusCode())
-                .entity(String.format("New counter state: %s", response.body()))
-                .build();
+                .POST(HttpRequest.BodyPublishers.ofString(""));
+        Span clientSpan = tracer.spanBuilder("POST proxy/counter").setSpanKind(SpanKind.CLIENT).startSpan();
+
+        try (Scope scope = clientSpan.makeCurrent()) {
+            openTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), reqBuilder, setter);
+            HttpResponse<String> response = client.send(reqBuilder.build(), HttpResponse.BodyHandlers.ofString());
+            return Response
+                    .status(response.statusCode())
+                    .entity(String.format("New counter state: %s", response.body()))
+                    .build();
+        } finally {
+            clientSpan.end();
+        }
     }
 
 }
--- tracing/opentelemetry/java/counter/src/main/java/Counter.java
+++ tracing/opentelemetry/java/counter/src/main/java/Counter.java
@@ -1,8 +1,16 @@
-import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.logging.Logger;
 
@@ -12,19 +20,57 @@ public class Counter {
 
     private int count = 0;
 
+    @Inject
+    private OpenTelemetry openTelemetry;
+
+    @Inject
+    private Tracer tracer;
+
+    TextMapGetter<HttpHeaders> getter = new TextMapGetter<>() {
+        @Override
+        public String get(HttpHeaders carrier, String key) {
+            if (carrier.getRequestHeaders().containsKey(key)) {
+                return carrier.getRequestHeader(key).get(0);
+            }
+            return null;
+        }
+
+        @Override
+        public Iterable<String> keys(HttpHeaders carrier) {
+            return carrier.getRequestHeaders().keySet();
+        }
+    };
+
     @GET
-    @WithSpan
     @Produces(MediaType.TEXT_PLAIN)
-    public String count() {
+    public String count(@jakarta.ws.rs.core.Context HttpHeaders headers) {
         LOG.info(String.format("count called, counter states: %d", count));
-        return String.valueOf(count);
+
+        Context extractedContext = openTelemetry.getPropagators().getTextMapPropagator().extract(Context.current(), headers, getter);
+        // Automatically use the extracted SpanContext as parent.
+        Span serverSpan = tracer.spanBuilder("GET counter/counter").setSpanKind(SpanKind.SERVER).startSpan();
+
+        try (Scope scope = extractedContext.makeCurrent()) {
+            return String.valueOf(count);
+        } finally {
+            serverSpan.end();
+        }
     }
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String increment() {
+    public String increment(@jakarta.ws.rs.core.Context HttpHeaders headers) {
         LOG.info(String.format("increment called, incrementing to %d", count + 1));
-        count++;
-        return String.valueOf(count);
+
+        Context extractedContext = openTelemetry.getPropagators().getTextMapPropagator().extract(Context.current(), headers, getter);
+        // Automatically use the extracted SpanContext as parent.
+        Span serverSpan = tracer.spanBuilder("POST counter/counter").setSpanKind(SpanKind.SERVER).startSpan();
+
+        try (Scope scope = extractedContext.makeCurrent()) {
+            count++;
+            return String.valueOf(count);
+        } finally {
+            serverSpan.end();
+        }
     }
 }
