40aee99c08312871f88f7812ff802d9f64554a5e
Vojtech Juranek <vojtech.juranek@gmail.com>
2023-08-28 14:30:27 +0200

    flink: kafka to kafka

---
+++ big-data/flink-kafka/src/main/java/io/github/vjuranek/FlinkKafka.java
@@ -0,0 +1,47 @@
+package io.github.vjuranek;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkKafka {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("test")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+        DataStreamSource<String> recordStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("test-out")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
+
+        recordStream.map(new LogMapper()).sinkTo(kafkaSink);
+
+        env.execute("Flink Kafka");
+    }
+
+    private static class LogMapper implements MapFunction<String, String> {
+        @Override public String map(String s) throws Exception {
+            return s.toUpperCase();
+        }
+    }
+}
